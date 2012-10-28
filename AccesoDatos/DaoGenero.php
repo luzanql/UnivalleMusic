5cc10f9adc28806ca17b802db2cd25c2adc45705
Jhonathan <jhota90@gmail.com>
2012-10-28 18:33:41 -0500

    modificacion dao genero

--- AccesoDatos/DaoGenero.php
+++ AccesoDatos/DaoGenero.php
@@ -1,16 +1,45 @@
 <?php
-include('conexion.php');
 
-class DaoGenero{
-  
-    function DaoGenero(){
-        $this->conexion = new Conexion();
-        $conexion->conectar();  
-        $sql="'SELECT nombre FROM Genero'"; 
+include('conexion.php');
+include('../Logica/Genero.php');
+
+class DaoGenero {
+
+    private $daoGenero;
+    private $genero;
+    private $codigo;
+
+    function DaoGenero() {
+        $this->daoGenero = new Conexion();
+    }
+
+    function getGeneros() {
+        $this->conexion->conectar();
+        $sql = "SELECT nombre FROM Genero";
         //ejecutando la consulta
         $respuesta = mysql_query($sql);
         return $row = mysql_fetch_object($respuesta);
     }
+    
+    function createGenero(Genero $g){
+   
+        $this->genero=$g;
+        $nombre=$g->getNombre();
+        $this->conexion->conectar();
+        $sql="INSERT INTO Genero VALUES ("+$nombre+")";
+        $ejecutar=  mysql_query($sql);
+        $this->conexion->cerrar();
         
+    }
+    
+    function deleteGenero($codigo){
+        $this->codigo=$codigo;
+        $sql="DELETE Genero WHERE codigo='"+$this->codigo+"'";
+        $ejecutar=  mysql_query($sql);
+        $this->conexion->cerrar();
+        
+    }
+    
+    
 }
 ?>
