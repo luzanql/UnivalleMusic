6b2a9bed33e669b49e249eed760693a82a31831a
Jhonathan <jhota90@gmail.com>
2012-10-28 15:44:13 -0500

    agregue paquete logica y le agregue al paquete la clase cancion.php

---
+++ Logica/Cancion.php
@@ -0,0 +1,28 @@
+<?php
+class Cancion {
+    
+    var $titulo;
+    var $codigo;
+    var $album;
+    var $genero;
+    
+    function setTitulo($titulo){
+        $this->titulo=$titulo;
+    }
+    
+    function setCodigo($codigo){
+        $this->codigo=$codigo;
+    }
+    
+    function setAlbum ($album){
+        $this->album=$album;               
+    }
+    
+    
+   
+    
+    
+    
+    
+}
+?>
