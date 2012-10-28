684cf1dc77db6bcf6cd95639bded265b36c42eea
DianaValencia <yo.dianamarcela@gmail.com>
2012-10-28 17:22:23 -0500

    Creacion DaoListaReproduccion

--- AccesoDatos/DaoListaReproduccion.php
+++ AccesoDatos/DaoListaReproduccion.php
@@ -2,6 +2,16 @@
 
 class DaoListaReproduccion {
     
+    private $conexion;
+    
+    function DaoListaReproduccion($con){
+        $this->conexion = $con;
+    }
+    
+    function create($lr){
+        $sqr ="INSERT INTO ListaReproduccion(nombre) VALUES ()";
+    }
+    
 }
 
 ?>
