90f190d181ba17ca3bcc2e7082b1348c0ce14f3c
Jhonathan <jhota90@gmail.com>
2012-11-04 16:32:07 -0500

    DaoListaReproduccion, ControladorListaReproduccion

--- Controladores/ControladorListaReproduccion.php
+++ Controladores/ControladorListaReproduccion.php
@@ -1,5 +1,35 @@
 <?php
-   class ControladorListaReproduccion{
-       
-   }
+require_once '../AccesoDatos/DaoListaReproduccion.php';
+require_once '../Logica/ListaReproduccion.php';
+
+class ControladorListaReproduccion {
+
+    private $daoListaReproduccion;
+
+    function ControladorListaReproduccion() {
+        $this->daoListaReproduccion = new DaoListaReproduccion();
+    }
+
+    function obtenerListasReproduccion() {
+       //Modificar con sesion
+        $idUsuario = 1;
+       //-----------
+        return $this->daoListaReproduccion->getListasReproduccionPorUsuario($idUsuario);
+    }
+    
+    function createListaReproduccion($nombre){
+        $listaReproduccion = new ListaReproduccion();
+        $listaReproduccion->setNombre($nombre);
+        //Modificar cuando se implement la sesion
+        $idUsuario = 1;
+        //--------------------
+        $listaReproduccion->setIdUsuario($idUsuario);
+        $this->daoListaReproduccion->createListaReproduccion($listaReproduccion);
+    }
+    
+    function deleteListaReproduccion($codigo){
+        $this->daoListaReproduccion->deleteListaReproduccion($codigo);
+    }
+
+}
 ?>
