2d3adcfa2a03abd7f0fa41e1c71ef921dad5ff82
maleja <mayra.tabares@gmail.com>
2012-11-10 12:41:32 -0500

    haciendo pruebas para el formulario de crear un nuevo usuario

--- AccesoDatos/conexion.php
+++ AccesoDatos/conexion.php
@@ -11,8 +11,8 @@ class Conexion {
     function Conexion() {
         $this->server = "localhost";
         $this->usuario = "root";
-        $this->password = "root";
-        $this->basedatos = "UnivalleMusic";
+        $this->password = "maleja";
+        $this->basedatos = "univallemusic";
       }
         
         
