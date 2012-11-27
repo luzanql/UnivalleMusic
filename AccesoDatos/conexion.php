048de86463ec6ab64af732d2e42de249a71f77ae
maleja <mayra.tabares@gmail.com>
2012-11-26 22:35:09 -0500

    cambio conexion datos

--- AccesoDatos/conexion.php
+++ AccesoDatos/conexion.php
@@ -11,7 +11,7 @@ class Conexion {
     function Conexion() {
         $this->server = "localhost";
         $this->usuario = "root";
-        $this->password = "mayraata";
+        $this->password = "maleja";
         $this->basedatos = "univallemusic";
       }
         
