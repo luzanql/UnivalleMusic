6fd4c4442e7cd37c615cb2cc2a9f1cf8a34c9748
Jhonathan <jhota90@gmail.com>
2012-12-09 17:16:03 -0500

    cuando doy click en la publicicdad me manda a comprar musica

---
+++ Recursos/Scripts/ManejaPublicidad.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+     var url="../Controladores/Carrito.php?opcion=4";     
+    $('#pagarFinal').click(function(){
+        alert('Dio cliock pagar');
+            $.ajax({
+        type: "POST",
+        url: url,
+            success: function( msg ) {
+                var pesos=(msg * 1950);
+                if(pesos<=0){
+                    $("#mensajeConfirmacion").text("Lo sentimos, el valor de su transaccion es inferior al monto minimo establecido");
+
+                } else {
+                    //ajax de guardar compra y canciones por compra
+                    var url2="../Controladores/Carrito.php?opcion=6&valor="+pesos;
+                    $.ajax({
+                        type: "POST",
+                        url: url2,
+                        success: function( msg ) {
+                            alert(msg);
+                            var url1="../Controladores/Carrito.php?opcion=5";
+                            $.ajax({
+                                type: "POST",
+                                url: url1,
+                                success: function( msg ) {}
+                            })
+                            $("#mensajeConfirmacion").text("El pago se realizo satisfactoriamente");
+                        }
+                    })
+               
+                }
+                
+        }
+    });
+            
+    });
+
+            
+
+
+});
+
+
+
+
+        
+        
+
+
